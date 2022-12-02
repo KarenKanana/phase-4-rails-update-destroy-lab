404513ebb539905ec77b8e76a7b81913dd43c410
Karen Kanana <kananakaren21@gmail.com>
2022-12-02 13:00:07 +0300

    Complete assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -17,6 +17,18 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+  #DELETE /plants/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
+  end
+  # PATCH/PUT /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant
+  end
 
   private
 
